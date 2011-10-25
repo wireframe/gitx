2ee88615c187cd8af79541e66874f3d600d2f8b6
Ryan Sonnek <ryan@socialcast.com>
2011-10-25 16:10:10 -0500

    fix exec

--- lib/socialcast-git-extensions/github.rb
+++ lib/socialcast-git-extensions/github.rb
@@ -3,13 +3,11 @@ require 'json'
 
 module Socialcast
   module Github
-    class << self
-      def create_pull_request(username, password, branch)
-        payload = {:title => branch, :base => 'master', :head => branch}.to_json
-        response = RestClient::Request.new(:url => "https://api.github.com/repos/socialcast/socialcast/pulls", :method => "POST", :user => username, :password => password, :payload => payload, :headers => {:accept => :json, :content_type => :json}).execute
-        data = JSON.parse response.body
-        url = data['html_url']
-      end
+    def create_pull_request(username, password, branch)
+      payload = {:title => branch, :base => 'master', :head => branch}.to_json
+      response = RestClient::Request.new(:url => "https://api.github.com/repos/socialcast/socialcast/pulls", :method => "POST", :user => username, :password => password, :payload => payload, :headers => {:accept => :json, :content_type => :json}).execute
+      data = JSON.parse response.body
+      url = data['html_url']
     end
   end
 end
