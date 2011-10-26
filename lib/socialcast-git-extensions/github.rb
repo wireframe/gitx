1452758d1fa764602d01cf3ab4685d138e234155
Ryan Sonnek <ryan@socialcast.com>
2011-10-26 16:13:44 -0500

    determine github repo from git config

--- lib/socialcast-git-extensions/github.rb
+++ lib/socialcast-git-extensions/github.rb
@@ -3,9 +3,9 @@ require 'json'
 
 module Socialcast
   module Github
-    def create_pull_request(username, password, branch)
+    def create_pull_request(username, password, branch, repo)
       payload = {:title => branch, :base => 'master', :head => branch}.to_json
-      response = RestClient::Request.new(:url => "https://api.github.com/repos/socialcast/socialcast/pulls", :method => "POST", :user => username, :password => password, :payload => payload, :headers => {:accept => :json, :content_type => :json}).execute
+      response = RestClient::Request.new(:url => "https://api.github.com/repos/#{repo}/pulls", :method => "POST", :user => username, :password => password, :payload => payload, :headers => {:accept => :json, :content_type => :json}).execute
       data = JSON.parse response.body
       url = data['html_url']
     end
