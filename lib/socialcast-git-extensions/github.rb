bb4e664c4859323b4db81de2c18e4e838c50640a
Geoffrey Hichborn <geoff@socialcast.com>
2012-01-13 13:58:53 -0800

    Replaced puts with Highline.say and now exits with status code 1 on failure

--- lib/socialcast-git-extensions/github.rb
+++ lib/socialcast-git-extensions/github.rb
@@ -6,13 +6,13 @@ module Socialcast
     def create_pull_request(username, password, branch, repo)
       payload = {:title => branch, :base => 'master', :head => branch}.to_json
       begin
-        puts "Creating pull request for #{branch} against master in #{repo}"
+        HighLine.say "Creating pull request for #{branch} against master in #{repo}"
         response = RestClient::Request.new(:url => "https://api.github.com/repos/#{repo}/pulls", :method => "POST", :user => username, :password => password, :payload => payload, :headers => {:accept => :json, :content_type => :json}).execute
         data = JSON.parse response.body
         url = data['html_url']
       rescue RestClient::Exception => e
         data = JSON.parse e.http_body
-        puts "Failed to create pull request: #{data['message']}"
+        HighLine.say "Failed to create pull request: #{data['message']}"
         false
       end
     end
--- lib/socialcast-git-extensions/version.rb
+++ lib/socialcast-git-extensions/version.rb
@@ -1,5 +1,5 @@
 module Socialcast
   module Gitx
-    VERSION = "2.3.1"
+    VERSION = "2.3.2"
   end
 end
