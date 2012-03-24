c2589d8794b3a3b304668723b3ff4c4f4f9924c8
Sean Cashin <scashin133@gmail.com>
2012-03-24 13:01:23 -0700

    this is how to configure proxy for restclient.

--- lib/socialcast-git-extensions/github.rb
+++ lib/socialcast-git-extensions/github.rb
@@ -7,7 +7,8 @@ module Socialcast
       payload = {:title => branch, :base => 'master', :head => branch, :body => body}.to_json
       begin
         HighLine.say "Creating pull request for #{branch} against master in #{repo}"
-        response = RestClient::Request.new(:url => "https://api.github.com/repos/#{repo}/pulls", :method => "POST", :user => username, :password => password, :payload => payload, :headers => {:accept => :json, :content_type => :json}, :proxy => ENV['HTTPS_PROXY']).execute
+        RestClient.proxy = ENV['HTTPS_PROXY'] if ENV.has_key?('HTTPS_PROXY')
+        response = RestClient::Request.new(:url => "https://api.github.com/repos/#{repo}/pulls", :method => "POST", :user => username, :password => password, :payload => payload, :headers => {:accept => :json, :content_type => :json}).execute
         data = JSON.parse response.body
         url = data['html_url']
       rescue RestClient::Exception => e
